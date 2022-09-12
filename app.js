12a7bd11d084fa8c64ea32eb7556353d3cc8d8d6
ozrn <ozerenkevser92@gmail.com>
2022-09-12 22:22:11 +0300

    Display elements on web site fetching items

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 const express = require("express");
+
 // https is not needed to install using node because it is one of the native node modules which is already bundled with on node module projects.
-const https = require('https');
-
-
+const https = require("https");
 
 const app = express();
 
-app.get("/", (req,res) => {
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?lat=48.8534&lon=2.3488&units=metric&appid=f6a2d47f42a50d41875a53111b7d8dd7";
+
+app.get("/", (req, res) => {
+
+  const query = "London";
+  const apiKey = "f6a2d47f42a50d41875a53111b7d8dd7";
+  const unit = "metric";
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=${unit}&appid=${apiKey}`;
 
   https.get(url, (response) => {
 
-  response.on('data', (data) => {
-    const weatherData = JSON.parse(data); // converting data into actual JS object!!
-    const currentTemp = weatherData.main.temp;
-    const weatherDesc = weatherData.weather[0].description;
-    console.log(currentTemp, weatherDesc);
+    response.on('data', (data) => {
+      const weatherData = JSON.parse(data); // converting data into actual JS object!!
+      const currentTemp = weatherData.main.temp;
+      const weatherDesc = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
+      // we can only have one res.send but have multiple res.write!!!!
+      res.write(`<p> The weather is currently ${weatherDesc}</p>`);
+      res.write(`<h1> The temperature in ${query} is ${currentTemp} degrees Celcius</h1> `);
+      res.write(`<img src ="${imageURL}">`);
+      res.send();
+
+    })
   })
-  })
-  res.send("Server is up and running")
+
 })
-
-
 
 
 
